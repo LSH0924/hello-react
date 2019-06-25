95a1bc5c32f9076286fd7b5ec7d2ad860a70748c
LSH0924 <tngus25866@gmail.com>
2019-06-25 23:58:10 +0900

    새 컴포넌트 만들기 실습

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class MyComponent extends Component {
+    render () {
+        return (
+            <div>
+                실습용 새 컴포넌트 만들기 - new!
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
