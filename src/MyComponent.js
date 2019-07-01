075e04b81a89ee1718c831591999b4de3de97bba
LSH0924 <tngus25866@gmail.com>
2019-07-01 23:17:15 +0900

    3.2.4.1 필수 props 설정하기

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -6,10 +6,12 @@ class MyComponent extends Component {
 
     static defaultProps={
         name: "이수현"
+        , catAge:9
     }
 
     static propTypes={
         name:PropTypes.string
+        , catAge: PropTypes.number.isRequired
     }
 
     render () {
@@ -18,6 +20,7 @@ class MyComponent extends Component {
                 실습용 새 컴포넌트 만들기 - new!
                 <h1>부모 컴포넌트에서 이름 받아오기</h1>
                 <p>안녕! 내 이름은 {this.props.name} 이라구 해!</p>
+                <p>내 고양이는 {this.props.catAge}살이야.</p>
             </div>
         )
     }
