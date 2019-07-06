ae29e5b4552f0616ea49f4bb7cc3fd1b55f628df
LSH0924 <tngus25866@gmail.com>
2019-07-06 22:54:28 +0900

    5.3.3. ref로 스크롤바 내리는 메서드 만들기

--- src/ScrollBox.js
+++ src/ScrollBox.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ScrollBox extends Component {
-    render() {
-        const style = {
-            border : "1px solid black",
-            height : "300px",
-            width : "300px",
-            overflow : "auto",
-            position : "relative"
-        }
+  buttonScrollDown = () => {
+    const { scrollHeight, clientHeight } = this.box;
+    // 안쪽 div 의 높이에서 바깥쪽 div의 높이를 뺀 값을 스크롤바 위치에 설정한다.
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
 
-        const innerStyle = {
-            width : "100%",
-            height : "650px",
-            background: "linear-gradient(lightcoral, lightgreen)"
-        };
+  render() {
+    const style = {
+      border: "1px solid black",
+      height: "300px",
+      width: "300px",
+      overflow: "auto",
+      position: "relative"
+    };
 
-        return (
-            <div
-            style={style}
-            ref={r => this.box = r}
-            >
-                <div style={innerStyle}/>
-            </div>
-        );
-    }
+    const innerStyle = {
+      width: "100%",
+      height: "650px",
+      background: "linear-gradient(lightcoral, lightgreen)"
+    };
+
+    return (
+      <div style={style} ref={r => (this.box = r)}>
+        <div style={innerStyle} />
+      </div>
+    );
+  }
 }
 
 export default ScrollBox;
