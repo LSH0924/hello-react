be5cee462cbda91e4a5836400099b35db70089c8
LSH0924 <tngus25866@gmail.com>
2019-07-06 23:15:50 +0900

    6.4.계절 더하기 기능 구현

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 
 class IterationSample extends Component {
-  render() {
-    const seasons = ["봄", "여름", "가을", "겨울"];
+  state = {
+    seasons: ["봄"],
+    season: ""
+  };
 
+  handleInputChange = e => {
+    this.setState({
+      season: e.target.value
+    });
+  };
+
+  handleButtonAdd = () => {
+    const { seasons, season } = this.state;
+    this.setState({
+      seasons: seasons.concat(season),
+      season: ""
+    });
+  };
+
+  render() {
+    const { seasons } = this.state;
     const seasonList = seasons.map((season, index) => (
       <li key={index}>{season}</li>
     ));
 
-    return <ul>{seasonList}</ul>;
+    return (
+      <div>
+        <h2>사계절</h2>
+        <ul>{seasonList}</ul>
+        <div>
+          <input
+            type="text"
+            name="season"
+            value={this.state.season}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleButtonAdd}>계절 추가하기</button>
+        </div>
+      </div>
+    );
   }
 }
 
