733b09091f16c4965a41b20c17e9616eeb462981
LSH0924 <tngus25866@gmail.com>
2019-07-15 21:57:19 +0900

    13.1.8 provider 를 이용한 리액트 애플리케이션과 스토어 연결

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 // 리덕스 관련 불러오기
-import {createStore} from "redux";
-import reducers from "./reducers"
+import { createStore } from "redux";
+import reducers from "./reducers";
+import {Provider} from "react-redux";
 
 // 스토어 생성
 const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const provider = 
+            <Provider store={store}>
+                <App/>
+            </Provider>;
+
+ReactDOM.render(provider, document.getElementById('root'));
 
 serviceWorker.unregister();
