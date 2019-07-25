1a04fa4862c7d2f21ae0e52610f8b0e6403e9c61
LSH0924 <tngus25866@gmail.com>
2019-07-25 23:33:49 +0900

    컴포넌트 컨테이너 수정

--- src/containers/CounterListContainer.js
+++ src/containers/CounterListContainer.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import CounterList from "../components/CounterList";
 import {connect} from "react-redux";
 import { bindActionCreators } from "redux";
 import * as counterActions from "../modules/counter";
+import * as postActions from "../modules/post";
 
 // 데이터와 함수들을 props로 받는 컴포넌트 생성후
 // 표시해 줄 컴포넌트를 넣어준다.
 
-const CounterListContainer = ({ counters, counterAction }) => {
+class CounterListContainer extends Component{
+
+render(){
+  const {counters, CounterAction} = this.props;
   return (
     <CounterList
-      counters={counters}
-      onIncreament={counterAction.increamentAcync}
-      onDecreament={counterAction.decreamentAcync}
-      onSetColor={counterAction.setColor}
+      state={counters}
+      onIncreament={CounterAction.increament}
+      onDecreament={CounterAction.decreament}
+      onSetColor={CounterAction.setColor}
     />
   );
+}
+
 };
 
 export default connect(
-  state => ({ counters: state }),
-  dispatch => ({ counterAction: bindActionCreators(counterActions, dispatch) })
+  state => ({ 
+    counters: state,
+    post: state.post.data,
+    loading: state.post.pending,
+    error: state.post.error
+  }),
+  dispatch => ({ 
+    CounterAction: bindActionCreators(counterActions, dispatch),
+    PostAction: bindActionCreators(postActions, dispatch) 
+  })
     )(CounterListContainer);
