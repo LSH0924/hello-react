d0ebeb10716b9799ee7434ab831a51e79b5459f6
LSH0924 <tngus25866@gmail.com>
2019-07-25 23:00:45 +0900

    15.2.2 리듀서 합치기 및 스토어에 적용

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 // 리덕스 관련 불러오기
 import { createStore, applyMiddleware } from "redux";
-import module from "./modules/counter";
+import modules from "./modules";
 import ReduxThunk from "redux-thunk";
 import logger from "redux-logger";
 
-export default createStore(module, applyMiddleware(logger, ReduxThunk));
+export default createStore(modules, applyMiddleware(logger, ReduxThunk));
