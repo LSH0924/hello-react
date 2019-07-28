bf9ae6d8a46b0d292ad668566b92828e1a183046
LSH0924 <tngus25866@gmail.com>
2019-07-28 11:23:31 +0900

    15.2.3. redux-promise-middleware 적용하기

--- src/modules/post.js
+++ src/modules/post.js
@@ -10,6 +10,7 @@ function getPostAPI(postId){
 }
 
 // 액션 타입 정의
+const GET_POST = "GET_POST";
 const GET_POST_PENDING = 'GET_POST_PENDING';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_FAILURE = 'GET_POST_FAILURE';
@@ -20,24 +21,29 @@ const getPostSuccess = createAction(GET_POST_SUCCESS);
 const getPostFailure = createAction(GET_POST_FAILURE);
 
 // thunk 만들기
-export const getPost = postId => dispatch => {
-    // 요청 시작을 알린 뒤, 서버에서 응답이 올 때까지 기다린다.
-    dispatch(getPostPending());
+// export const getPost = postId => dispatch => {
+//     // 요청 시작을 알린 뒤, 서버에서 응답이 올 때까지 기다린다.
+//     dispatch(getPostPending());
 
-    return getPostAPI(postId)
-    .then(response => {
-        // axios로 보낸 요청이 성공일 경우 서버 응답 내용을 payload로 설정한다.
-        dispatch(getPostSuccess(response));
-        // 나중에 getPostApi.then(이 안에 들어가는 화살표 함수) 에서 response를 쓸 수 있게 return 해준다.
-        return response;
-    })
-    .catch(error => {
-        // error 내용을 payload로 설정 후 디스패치한다.
-        dispatch(getPostFailure(error));
-        // error를 throw 하면 위와같이 getPostApi.catch(이 안에 들어가는 화살표 함수)에서 다시 한번 사용할 수 있다.
-        throw(error);
-    });
-};
+//     return getPostAPI(postId)
+//     .then(response => {
+//         // axios로 보낸 요청이 성공일 경우 서버 응답 내용을 payload로 설정한다.
+//         dispatch(getPostSuccess(response));
+//         // 나중에 getPostApi.then(이 안에 들어가는 화살표 함수) 에서 response를 쓸 수 있게 return 해준다.
+//         return response;
+//     })
+//     .catch(error => {
+//         // error 내용을 payload로 설정 후 디스패치한다.
+//         dispatch(getPostFailure(error));
+//         // error를 throw 하면 위와같이 getPostApi.catch(이 안에 들어가는 화살표 함수)에서 다시 한번 사용할 수 있다.
+//         throw(error);
+//     });
+// };
+
+export const getPost = postId => ({
+    type: GET_POST,
+    patyload: getPostAPI(postId)
+});
 
 const initialState = {
     pending: false,
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 // 리덕스 관련 불러오기
 import { createStore, applyMiddleware } from "redux";
 import modules from "./modules";
-import ReduxThunk from "redux-thunk";
+// import ReduxThunk from "redux-thunk";
+import promiseMiddleware from "redux-promise-middleware";
+
 import logger from "redux-logger";
 
-export default createStore(modules, applyMiddleware(logger, ReduxThunk));
+export default createStore(modules, applyMiddleware(logger, promiseMiddleware()));
